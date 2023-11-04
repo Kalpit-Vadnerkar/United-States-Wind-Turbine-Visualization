de7c9530fa48a92b752a22c84b303c403e361bf4
sorliog <sorliog@clemson.edu>
2023-11-04 17:20:23 -0400

    Finished map

--- docs/script.js
+++ docs/script.js
@@ -54,17 +54,188 @@ const stateNameMapping = {
 function generateVisualization1() {
     let element = document.getElementById("viz1");
 
-    var width = element.clientWidth;
-    var height = element.clientHeight;
+    const width = element.clientWidth;
+    const height = element.clientHeight;
+    const colors = ["#26440e", "#66ff5b"];
 
+    function drawLegend(svg, range) {
+        // Create the gradient first
+        var gradient = svg.append("svg:defs")
+            .append("svg:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "0%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad")
+            .attr("gradientTransform", "rotate(45)");
+        gradient.append("svg:stop")
+            .attr("offset", "0%")
+            .attr("stop-color", colors[0])
+            .attr("stop-opacity", 1);
+
+        gradient.append("svg:stop")
+            .attr("offset", "100%")
+            .attr("stop-color", colors[1])
+            .attr("stop-opacity", 1);
+
+
+        // Start the legend
+        const legend = svg
+            .append("g")
+            .attr("class", "legend")
+        legend.append("rect")
+            .attr("fill", "rgba(155,155,155,0.8)")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", 300)
+            .attr("height", 200)
+            .attr("stroke", "lightgray")
+            .attr("stroke-width", 1);
+
+        // Legend title
+        legend.append("text")
+            .attr("x", 120)
+            .attr("y", 20)
+            .attr("class", "legend-title")
+            .attr("font-size", "24px")
+            .text("Legend");
+
+        // Color gradient
+        const gradientLegend = legend
+            .append("g")
+            .selectAll("rect")
+            .data([1])
+            .enter()
+            .append("rect")
+            .attr("fill", "url(#gradient)")
+            .attr("x", 40)
+            .attr("y", 30)
+            .attr("width", 30)
+            .attr("height", 120)
+            .attr("stroke", "lightgray")
+            .attr("stroke-width", 1);
+
+        const gradientLabel = legend.append("text")
+            .attr("transform", "translate(65, 80)")
+            .selectAll("tspan")
+            .data(["Number of", "Turbine Projects"])
+            .enter()
+            .append("tspan")
+            .attr("x", 15)
+            .attr("dy", 15)
+            .text(d => {
+                return d;
+            });
+
+
+        const gradientLabels = legend
+            .append("g")
+            .selectAll("text")
+            .data(range)
+            .enter()
+            .append("text")
+            .attr("fill", d => {
+                return "black";
+            })
+            .attr("x", 33 + 40)
+            .attr("y", d => {
+                return range.indexOf(d) * 100 + 50
+            })
+            .text(d => {
+                return d;
+            });
+
+        // Point key
+        const pointLegend = legend
+            .append("g")
+            .selectAll("circle")
+            .data([1])
+            .enter()
+            .append("circle")
+            .attr("r", 3)
+            .attr("fill", "#63b7b7")
+            .attr("stroke", "#153b3b")
+            .attr("stroke-width", 1)
+            .attr("transform", d => {
+                if (d != null)
+                    return "translate(58, 165)";
+            });
+
+        const pointLabel = legend
+            .append("text")
+            .attr("transform", "translate(63, 155)")
+            .selectAll("tspan")
+            .data(["Location of", "Turbine Projects"])
+            .enter()
+            .append("tspan")
+            .attr("x", 15)
+            .attr("dy", 15)
+            .text(d => {
+                return d;
+            });
+
+        legend.attr("transform", "translate(" + width * 0.7 + "," + ((height / 2) - 90) + ")");
+    }
+
+
+    function drawMapAndTurbines(svg, mapData, turbineData, projection, range) {
+
+
+        let pathGenerator = d3.geoPath(projection);
+        let path = svg.append("path")
+            .attr("d", pathGenerator({type: "Sphere"}))
+            .attr("stroke", "gray")
+            .attr("fill", "lightblue");
+
+
+        let colorScale = d3.scaleLinear().domain(range).range(colors);
+
+        let states = svg.append("g")
+            .selectAll(".state")
+            .data(mapData.features)
+            .enter()
+            .append("path")
+            .attr("class", "state")
+            .attr("stroke", "lightgray")
+            .attr("stroke-width", 1)
+            .attr("fill", d => {
+                if (countByState[d.properties.NAME] != null)
+                    return colorScale(countByState[d.properties.NAME]);
+                return "darkgray"
+            })
+            .attr("d", d => pathGenerator(d))
+
+        let points = svg.append("g")
+            .selectAll(".point")
+            .data(turbineData)
+            .enter()
+            .append("circle")
+            .attr("r", 3)
+            .attr("fill", "#63b7b7")
+            .attr("stroke", "#153b3b")
+            .attr("stroke-width", 1)
+            .attr('transform', d => {
+                let coords = projection([d.xlong, d.ylat]);
+                return "translate(" + projection([d.xlong, d.ylat]) + ")";
+            });
+    }
 
     var svg = d3.select("#viz1").attr("width", width).attr("height", height);
 
     d3.csv("uswtdb_v6_0_20230531.csv").then(turbineData => {
             d3.json("gz_2010_us_040_00_500k.json").then(mapData => {
 
+                // Create the projection
+                let projection = d3.geoAlbersUsa().fitWidth(width * 0.7, {type: "Sphere"});
 
-                // First, count how many turbines in each state
+                // If the point is not within the frame of the projection, filter it out
+                turbineData = turbineData.filter(d => {
+                    let coords = projection([d.xlong, d.ylat]);
+                    return coords != null;
+                });
+
+                // Count how many turbines in each state
                 countByState = {}
                 for (const turbineDatum of turbineData) {
 
@@ -85,132 +256,12 @@ function generateVisualization1() {
                     minCount = Math.min(countByState[state], minCount);
 
                 }
-
-
-                let projection = d3.geoAlbersUsa().fitWidth(width * 0.7, {type: "Sphere"});
-
-                let pathGenerator = d3.geoPath(projection);
-                let path = svg.append("path")
-                    .attr("d", pathGenerator({type: "Sphere"}))
-                    .attr("stroke", "gray")
-                    .attr("fill", "lightblue");
-
                 let range = [minCount, maxCount];
-                let colors = ["#26440e", "#66ff5b"];
-                let colorScale = d3.scaleLinear().domain(range).range(colors);
 
-                let state = svg.append("g")
-                    .selectAll(".state")
-                    .data(mapData.features)
-                    .enter()
-                    .append("path")
-                    .attr("class", "state")
-                    .attr("stroke", "lightgray")
-                    .attr("stroke-width", 1)
-                    .attr("fill", d => {
-                        if (countByState[d.properties.NAME] != null)
-                            return colorScale(countByState[d.properties.NAME]);
-                        return "darkgray"
-                    })
-                    .attr("d", d => pathGenerator(d))
+                // Draw data
+                drawMapAndTurbines(svg, mapData, turbineData, projection, range);
+                drawLegend(svg, range);
 
-                let points = svg.append("g")
-                    .selectAll(".point")
-                    .data(turbineData)
-                    .enter()
-                    .append("circle")
-                    .attr("r", 3)
-                    .attr("fill", "#63b7b7")
-                    .attr("stroke", "#153b3b")
-                    .attr("stroke-width", 1)
-                    .attr("transform", d => {
-                        if (d != null)
-                            return "translate(" + projection([d.xlong, d.ylat]) + ")";
-                    });
-
-
-                var gradient = svg.append("svg:defs")
-                    .append("svg:linearGradient")
-                    .attr("id", "gradient")
-                    .attr("x1", "0%")
-                    .attr("y1", "0%")
-                    .attr("x2", "100%")
-                    .attr("y2", "100%")
-                    .attr("spreadMethod", "pad")
-                    .attr("gradientTransform", "rotate(45)");
-                gradient.append("svg:stop")
-                    .attr("offset", "0%")
-                    .attr("stop-color", colors[0])
-                    .attr("stop-opacity", 1);
-
-                gradient.append("svg:stop")
-                    .attr("offset", "100%")
-                    .attr("stop-color", colors[1])
-                    .attr("stop-opacity", 1);
-
-
-                const legend = svg
-                    .append("g")
-                    .attr("class", "legend")
-                legend.append("rect")
-                    .attr("fill", "rgba(155,155,155,0.8)")
-                    .attr("x", 0)
-                    .attr("y", 0)
-                    .attr("width", 120)
-                    .attr("height", 180)
-                    .attr("stroke", "lightgray")
-                    .attr("stroke-width", 1);
-
-                const gradientLegend = legend
-                    .append("g")
-                    .selectAll("rect")
-                    .data([1])
-                    .enter()
-                    .append("rect")
-                    .attr("fill", "url(#gradient)")
-                    .attr("x", 40)
-                    .attr("y", 30)
-                    .attr("width", 30)
-                    .attr("height", 120)
-                    .attr("stroke", "lightgray")
-                    .attr("stroke-width", 1);
-
-                const gradientLabel = legend.append("text").text("Number of Turbine Projects").attr("x", 0).attr("y", 20);
-                const gradientLabels = legend
-                    .append("g")
-                    .selectAll("text")
-                    .data(range)
-                    .enter()
-                    .append("text")
-                    .attr("fill", d => {
-                        return "black";
-                    })
-                    .attr("x", 33 + 40)
-                    .attr("y", d => {
-                        return range.indexOf(d) * 100 + 50
-                    })
-                    .text(d => {
-                        return d;
-                    });
-
-                const pointLegend = legend
-                    .append("g")
-                    .selectAll("circle")
-                    .data(range)
-                    .enter()
-                    .append("text")
-                    .attr("fill", d => {
-                        return "black";
-                    })
-                    .attr("x", 33 + 40)
-                    .attr("y", d => {
-                        return range.indexOf(d) * 100 + 50
-                    })
-                    .text(d => {
-                        return d;
-                    });
-
-                legend.attr("transform", "translate(" + width * 0.7 + "," + ((height/2) - 90)  + ")");
             })
 
         }
